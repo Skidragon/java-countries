3989db3f49c1fafccb9779c77fb246865b6534b9
Simon Davis <simon_freelance@hotmail.com>
2019-02-11 18:53:53 -0500

    Finished age of countries routes.

--- src/main/java/com/lambdaschool/countrysearch/CountryAgeController.java
+++ src/main/java/com/lambdaschool/countrysearch/CountryAgeController.java
@@ -1,22 +1,45 @@
 package com.lambdaschool.countrysearch;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/age")
 public class CountryAgeController {
 
-//    @RequestMapping("age")
-//    public ArrayList<Country> getCountriesWithPopulationSize(@RequestParam(value="age") int population) {
-//
-//    }
-//    @RequestMapping("min")
-//    public Country getCountryWithSmallestPopulation() {
-//
-//    }
-//    @RequestMapping("max")
-//    public Country getCountryWithLargestPopulation() {
-//
-//    }
+    @RequestMapping("/age")
+    public ArrayList<Country> getCountriesWithPopulationSize(@RequestParam(value="people") int population) {
+        ArrayList<Country> newCountries = new ArrayList<>();
+        CountrySearchApplication.myCountryList.countryList.forEach(c -> {
+            if(population <= c.getPopulation()) {
+                newCountries.add(c);
+            }
+        });
+        return newCountries;
+    }
+    @RequestMapping("/min")
+    public Country getCountryWithSmallestPopulation() {
+        int smallestIndex = 0;
+        ArrayList<Country> countries = CountrySearchApplication.myCountryList.countryList;
+        for(int i = 0; i < countries.size(); i++) {
+            if(countries.get(i).getPopulation() < countries.get(smallestIndex).getPopulation()) {
+                smallestIndex = i;
+            }
+        }
+        return countries.get(smallestIndex);
+    }
+    @RequestMapping("/max")
+    public Country getCountryWithLargestPopulation() {
+        int biggestIndex = 0;
+        ArrayList<Country> countries = CountrySearchApplication.myCountryList.countryList;
+        for(int i = 0; i < countries.size(); i++) {
+            if(countries.get(i).getPopulation() > countries.get(biggestIndex).getPopulation()) {
+                biggestIndex = i;
+            }
+        }
+        return countries.get(biggestIndex);
+    }
 }
